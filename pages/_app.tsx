2fa6418bdaf2861b36ed3fdc10db832c6e5d07d9
Brian Foody <brianfoody@gmail.com>
2022-12-12 18:13:48 +0800

    use preserve scroll for the client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { usePreserveScroll } from "../hooks/usePreserveScroll";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  usePreserveScroll();
+
+  return <Component {...pageProps} />;
 }
